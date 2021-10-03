fcf79e279147a312378f78b34b815582f36e45f1
Artur <77436065+artur-asimov@users.noreply.github.com>
2021-10-03 16:38:19 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
-/*  Lua Bot                                     */
-/*  Author - Artur Asimov                       */
-/*  Github - https://github.com/artur-asimov/lua-bot-js    */
+/*  Lua Bot                                                 */
+/*  Author - Artur Asimov                                   */
+/*  Github - https://github.com/artur-asimov/lua-bot-js     */
 
-const config = require('./config.json')
-const { Client, Intents, MessageEmbed } = require('discord.js');
+const { Client, Intents, MessageEmbed, Collection, CommandInteraction } = require('discord.js');
 const client = new Client({ intents:  [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] });
+
+const fs = require('fs');
+const config = require('./config.json')
 const PREFIX = config.prefix;
 
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
 
 // "You don't have permission to do that"
 const permissionEmbed = new MessageEmbed()
@@ -30,69 +38,26 @@ client.on('messageCreate', (message) => {
             .split(/\s+/);
         console.log(`[${message.guild.name}:${message.channel.name}] -> [${message.author.tag}]: used command - '${CMD_NAME}' with arguments '${args}'`);
 
-        // commands
+        // Commands
+        // command code you can get in commands/ folder
         if(CMD_NAME === 'ping')
-            message.reply('Pong!');
-        else if(CMD_NAME === 'info') {
-            const embed = new MessageEmbed()
-                .setColor('#0099ff')
-                .setTitle('Server Information')
-                .addFields(
-                    { name: 'Server name', value: `${message.guild.name}` },
-                    { name: 'Member count', value: `${message.guild.memberCount}` }
-                )
-            message.reply({ embeds: [embed] });
-        } else if(CMD_NAME === 'kick') {
-            if(!message.member.permissions.has('KICK_MEMBERS', false)) return message.reply({embeds: [permissionEmbed]});
-            if(args.length === 0) return message.reply('Please provide user to kick.');
-            var victim = message.mentions.members.first();
-            if(victim) {
-                victim.kick()
-                    .then((victim) => message.channel.send(`${victim} was kicked from the server.\nReason - ${args[1]}`))
-                    .catch(() => message.reply('I can\'t kick that user!'));
-                victim.send(`You has been **kicked** from ${message.guild.name}!\nReason - ${args[1]}`).catch(() => null);
-            }
-            else
-                message.reply('Undefined user!');
-        } else if(CMD_NAME === 'ban') {
-            if(!message.member.permissions.has('BAN_MEMBERS', false)) return message.reply({embeds: [permissionEmbed]});
-            if(args.length === 0) return message.reply('Please provide user to ban.');
-            var victim = message.mentions.members.first();
-            if(victim) {
-                victim.ban()
-                    .then((victim) => message.channel.send(`${victim} was banned from the server.\nReason - ${args[1]}`))
-                    .catch(() => message.reply('I can\'t ban that user!'));
-                victim.send(`You has been **banned** from the ${message.guild.name}!\nReason - ${args[1]}`).catch(() => null);
-            }
-            else
-                message.reply('Undefined user!');
-        } else if(CMD_NAME === 'clear') {
-            if(!message.member.permissions.has('KICK_MEMBERS', false)) return message.reply({embeds: [permissionEmbed]});
-            if(args.length === 0) return message.channel.send('Please provide a message count to delete.');
-                message.channel.bulkDelete(parseInt(args[0])+1).catch(() => null);
-        } else if(CMD_NAME === 'help') { 
-            const embed = new MessageEmbed()
-                .setColor('#0099ff')
-                .setTitle('Bot help')
-                .addFields(
-                    { name: 'Get help', value: `${PREFIX}help` },
-                    { name: 'Ping bot', value: `${PREFIX}ping` },
-                    { name: 'Get current server info', value: `${PREFIX}info`},
-                    { name: 'Kicking user', value: `${PREFIX}kick <user>`},
-                    { name: 'Banning user', value: `${PREFIX}ban <user>`},
-                    { name: 'Cleans the chat', value: `${PREFIX}clear <message count>`}
-                )
-            message.reply({ embeds: [embed] });
-        } else if(CMD_NAME === 'bebra') {
-            message.delete();
-            message.channel.send('https://tenor.com/view/%D0%B1%D0%B5%D0%B1%D1%80%D0%B0-bebra-gif-22576166');
-        } else {
-            const embed = new MessageEmbed()
-            .setColor('#0099ff')
-            .setTitle('Unknown command')
-            .setDescription(`Enter ${PREFIX}help to get list of commands!`)
-            message.reply({ embeds: [embed] });
-        }
+            client.commands.get('ping').execute(message, args);
+        else if(CMD_NAME === 'info')
+            client.commands.get('info').execute(message, args);
+        else if(CMD_NAME === 'kick')
+            client.commands.get('kick').execute(message, args);
+        else if(CMD_NAME === 'ban')
+            client.commands.get('ban').execute(message, args);
+        else if(CMD_NAME === 'clear')
+            client.commands.get('clear').execute(message, args);
+        else if(CMD_NAME === 'say')
+            client.commands.get('say').execute(message, args);
+        else if(CMD_NAME === 'help')
+            client.commands.get('help').execute(message, args);
+        else if(CMD_NAME === 'bebra')
+            client.commands.get('bebra').execute(message, args);
+        else
+            client.commands.get('unknown').execute(message, args);
     }
 });
 
