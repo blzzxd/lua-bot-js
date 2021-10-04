901e8ab62f75d4e83c72fac7973f5156d22ff4cb
Artur <77436065+artur-asimov@users.noreply.github.com>
2021-10-04 18:48:16 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -13,11 +13,13 @@ module.exports = {
                 { name: 'Get help', value: `${PREFIX}help` },
                 { name: 'Ping bot', value: `${PREFIX}ping` },
                 { name: 'Get current server info', value: `${PREFIX}info`},
+                { name: 'Get your avatar image', value: `${PREFIX}avatar`},
                 { name: 'Kicking user', value: `${PREFIX}kick <user>`},
                 { name: 'Banning user', value: `${PREFIX}ban <user>`},
+                { name: 'Mutes user', value: `${PREFIX}mute <user>`},
+                { name: 'Unmutes user', value: `${PREFIX}unmute <user>`},
                 { name: 'Cleans the chat', value: `${PREFIX}clear <message count>`},
-                { name: 'Bot says your message in the chat', value: `${PREFIX}say <message>`},
-                { name: 'Get your avatar image', value: `${PREFIX}avatar`}
+                { name: 'Bot says your message in the chat', value: `${PREFIX}say <message>`}
             )
         message.reply({ embeds: [embed] });
     }
